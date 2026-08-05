2f509ca0bbb6aa65faa05bddbf6c43d58d3dffd3
Shubhra Dutt <shubhradutt25@gmail.com>
2026-08-05 17:16:53 +0530

    Time: 21 ms (8.93%) | Memory: 13.6 MB (39.38%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        count={}
+        for n in nums:
+            if n in count:
+                count[n]+=1
+            else:
+                count[n]=1
+            if count[n]> len(nums)/2:
+                return n
