274de394eae93b53ee44440636e8b6f135598801
Shubhra Dutt <shubhradutt25@gmail.com>
2026-09-09 00:08:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (35.04%) - LeetHub-Neo

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        def dfs(node, current_sum):
+            if not node:
+                return 0
+            
+            current_sum = current_sum * 10 + node.val
+            
+            if not node.left and not node.right:
+                return current_sum
+            
+            return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+            
+        return dfs(root, 0)
